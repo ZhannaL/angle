823d9448b483b26b0d8b7a3d4a5a50ce29e76c7e
ZhannaL <zhannet1814@gmail.com>
2017-04-24 12:02:06 +0300

    Update Basket.js

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -5,8 +5,9 @@ class Basket extends Component {
 	render() {
 		return (
 			<div className="basket">
-				<p>sometext for example</p>
-				<a>sometext for example</a>
+				<p>sometext for example and chanches</p>
+			<b>sometext for example and conflict</b>
+				<a>sometext for example and conflict</a>
 				<div className="">
 					<div className="example">
 						<p>sometext for example</p>
