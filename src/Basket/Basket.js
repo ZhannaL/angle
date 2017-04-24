c1b0d676ec99a6899cf83528c1cbd6de70893299
ZhannaL <zhannet1814@gmail.com>
2017-04-24 11:40:27 +0300

    Update Basket.js
    chanches file for examples

--- src/Basket/Basket.js
+++ src/Basket/Basket.js
@@ -5,12 +5,12 @@ class Basket extends Component {
 	render() {
 		return (
 			<div className="basket">
-				<div className="">
-
-				</div>
+				<p>sometext for example</p>
+				<a>sometext for example</a>
 				<div className="">
 					<div className="example">
 						<p>sometext for example</p>
+			
 					</div>
 				</div>
 			</div>
